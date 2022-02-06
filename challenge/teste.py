66d087b4b694dda2fe0d058ac4e98a86c3ad5a1e
Jefferson Thiago <jeffersonthiago98@hotmail.com>
2022-02-05 22:58:32 -0300

    adicionado projeto final

---
+++ challenge/teste.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+
+navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+navegador.get("https://www.rpachallenge.com/?lang=EN")
+
+#carregando a tabela do excel em uma variável tabela
+tabela = pd.read_excel("challenge.xlsx")
+
+#recuperando as informações de cada linha da tabela
+for i, email in enumerate(tabela["Email"]):
+	firstName = tabela.loc[i,"First Name"]
+	lastName = tabela.loc[i,"Last Name "]
+	companyName = tabela.loc[i,"Company Name"]
+	roleInCompany = tabela.loc[i,"Role in Company"]
+	adress = tabela.loc[i,"Address"]
+	phoneNumber = tabela.loc[i,"Phone Number"]
+
+	#recuperando o label de cada campo do formulário, pegando o input mais próximo desse label e setando com cada linha da tabela
+	navegador.find_element(By.XPATH,".//label[text() = 'Phone Number']/following-sibling::input").send_keys(str(phoneNumber))
+	navegador.find_element(By.XPATH,".//label[text() = 'Email']/following-sibling::input").send_keys(email)
+	navegador.find_element(By.XPATH,".//label[text() = 'Address']/following-sibling::input").send_keys(adress)
+	navegador.find_element(By.XPATH,".//label[text() = 'Role in Company']/following-sibling::input").send_keys(roleInCompany)
+	navegador.find_element(By.XPATH,".//label[text() = 'Company Name']/following-sibling::input").send_keys(companyName)
+	navegador.find_element(By.XPATH,".//label[text() = 'Last Name']/following-sibling::input").send_keys(lastName)
+	navegador.find_element(By.XPATH,".//label[text() = 'First Name']/following-sibling::input").send_keys(firstName)
+	navegador.find_element(By.XPATH,"/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input").click()
+
+
