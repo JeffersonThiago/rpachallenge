c32b51c357a4b26d8495d919d1de123c19bc81a1
Jefferson Thiago <62192046+JeffersonThiago@users.noreply.github.com>
2022-02-06 15:56:48 -0300

    atualizações do start e captura de informação

--- challenge/teste.py
+++ challenge/teste.py
@@ -11,6 +11,9 @@ navegador.get("https://www.rpachallenge.com/?lang=EN")
 #carregando a tabela do excel em uma variável tabela
 tabela = pd.read_excel("challenge.xlsx")
 
+
+navegador.find_element(By.XPATH,"/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button").click()
+
 #recuperando as informações de cada linha da tabela
 for i, email in enumerate(tabela["Email"]):
 	firstName = tabela.loc[i,"First Name"]
@@ -30,4 +33,14 @@ for i, email in enumerate(tabela["Email"]):
 	navegador.find_element(By.XPATH,".//label[text() = 'First Name']/following-sibling::input").send_keys(firstName)
 	navegador.find_element(By.XPATH,"/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input").click()
 
-
+#recupera o resultado do teste, salva e atualiza o arquivo texto.txt
+texto1 = navegador.find_element(By.XPATH,"/html/body/app-root/div[2]/app-rpa1/div/div[2]/div[1]").text
+texto2 = navegador.find_element(By.XPATH,"/html/body/app-root/div[2]/app-rpa1/div/div[2]/div[2]").text
+f = open('texto.txt' , 'w')
+f.write(texto1)
+f.write(texto2)
+f.close()
+#imprime no prompt a informação salva
+f = open('texto.txt' , 'r')
+print(f.read())
+f.close()
